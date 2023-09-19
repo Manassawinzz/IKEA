499785e6fe7425b595055a1cb210694faca1995b
BikeSupakritJulamanee <118983249+BikeSupakritJulamanee@users.noreply.github.com>
2023-09-19 11:06:08 +0700

    19/9

--- project/src/administrator/AdminHomePage.jsx
+++ project/src/administrator/AdminHomePage.jsx
@@ -2,12 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { Container, Card, Image, Button, Form } from 'react-bootstrap';
 import Nav from './Nav';
 import { Link } from 'react-router-dom';
-
 import { storageRef, db } from '../firebase';
 import { ref, getDownloadURL, listAll } from 'firebase/storage';
-
 import { getDocs, collection, query, where, orderBy } from 'firebase/firestore';
-
 import './style/AdminHomePage.css';
 
 function Home() {
@@ -17,22 +14,11 @@ function Home() {
     const [products, setProducts] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
 
-    const findImage = (imgName) => {
-        return imageList.find((url) => url.includes(imgName));
-    };
-
     useEffect(() => {
         listAll(imageListRef)
-            .then((response) => {
-                const urlPromises = response.items.map((item) => getDownloadURL(item));
-                return Promise.all(urlPromises);
-            })
-            .then((urls) => {
-                setImageList(urls);
-            })
-            .catch((error) => {
-                console.error('Error listing images:', error);
-            });
+            .then((response) => Promise.all(response.items.map((item) => getDownloadURL(item))))
+            .then((urls) => setImageList(urls))
+            .catch((error) => console.error('Error listing images:', error));
     }, []);
 
     useEffect(() => {
@@ -48,54 +34,56 @@ function Home() {
             );
 
             const querySnapshot = await getDocs(q);
-            const newData = querySnapshot.docs.map((doc) => ({
-                ...doc.data(),
-                id: doc.id,
-            }));
-            setProducts(newData); // Set products as an array of filtered products
+            const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+            setProducts(newData);
         } catch (error) {
-            console.error('Error fetching account data: ', error);
+            console.error('Error fetching account data:', error);
         }
-    };
-
-    const handleSearch = () => {
-        fetchProducts();
     };
 
     return (
         <>
             <Nav />
             <Container>
-                <Form.Group className='search_group' >
-                    <Form.Control className='search_bar'
+                <Form.Group className='search_group'>
+                    <Form.Control
+                        className='search_bar'
                         type="text"
                         placeholder="Search by product name"
                         value={searchTerm}
                         onChange={(e) => setSearchTerm(e.target.value)}
                     />
-                    <Button className='search_btn' onClick={handleSearch}>Search</Button>
+                    <Button className='search_btn' onClick={fetchProducts}>find</Button>
                 </Form.Group>
                 <hr />
                 {products.map((product, index) => (
-                    <Card className="card" key={index}>
-                        <Image src={findImage(product.img)} style={{ width: '200px', height: '230px' }} />
-                        <Card.Body>
-                            <Card.Title>{product.name}</Card.Title>
-                            <Card.Text>จำนวน: {product.quantity}</Card.Text>
-                            <Link
-                                to={`/edit_products?id=${encodeURIComponent(product.id)}&name=${encodeURIComponent(
-                                    product.name
-                                )}&quantity=${encodeURIComponent(product.quantity)}&description=${encodeURIComponent(
-                                    product.description
-                                )}&image=${encodeURIComponent(product.image)}&price=${encodeURIComponent(
-                                    product.price
-                                )}`}
-                                target="_blank"
-                            >
-                                แก้ไข
-                            </Link>
-                        </Card.Body>
-                    </Card>
+                    <Link
+                        to={`/edit_products?id=${encodeURIComponent(product.id)}&name=${encodeURIComponent(
+                            product.name
+                        )}&quantity=${encodeURIComponent(product.quantity)}&description=${encodeURIComponent(
+                            product.description
+                        )}&image=${encodeURIComponent(product.image)}&price=${encodeURIComponent(
+                            product.price
+                        )}`}
+                        target="_blank"
+                        key={index}
+                    >
+                        <Card className="card">
+                            <Image
+                                className='img'
+                                src={imageList.find((url) => url.includes(product.img))}
+                                style={{ width: '290px', height: '300px' }}
+                            />
+                            <Card.Body>
+                                <div className='product_name'>{product.name}</div>
+                                <div className='product_description'>{product.description}</div>
+                                <div>
+                                    <span className='product_price'>{product.price.toLocaleString()}</span>
+                                    <b className='bath'>บาท</b>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Link>
                 ))}
             </Container>
         </>
